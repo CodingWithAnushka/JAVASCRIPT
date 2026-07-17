69c6dd5bbfd14e4f0a3cfe85cf248825f745c997
Anushka Rahul Surywanshi <jamkandeanushka@gmail.com>
2026-07-17 05:13:05 +0000

    add this file sum.js

---
+++ sum.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+
+let a = Number(prompt("Enter first number: "));
+let b = Number(prompt("Enter second number: "));
+
+let sum = a + b;
+
+console.log("Sum = " + sum);
